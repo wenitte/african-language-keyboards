b1deab160f855b85de9de1660f88b3b7f460cc00
Wenitte Apiou <wenitteapiou@gmail.com>
2022-08-28 12:14:22 -0400

    added script switch

--- src/Components/ScriptSelectDropDown/ScriptSelectDropDown.js
+++ src/Components/ScriptSelectDropDown/ScriptSelectDropDown.js
@@ -2,11 +2,12 @@ import React, { Component,  useEffect, useState, useRef, } from "react";
 import  {useParams, Link} from "react-router-dom";
 import {getLanguageNameFromCode} from "../../Utils/utils.js";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import {Button, Row, Col} from 'react-bootstrap';
+import scripts from "../../Scripts/scripts.js"
 import Select from 'react-select';
 
 
-let scripts = [{label:"N'ko", value:"Nko"}, {label:"Adlam", value:"Adlam"} ]
 
 
 function ScriptSelectDropdown(props) {
@@ -27,20 +28,71 @@ function ScriptSelectDropdown(props) {
       window.location.href = "/write/" + selectedOption.value + "/";
     }
   }
+
+    {/* //  <Button style= {{backgroundColor:"transparent", color:"white", borderColor:"white",borderRadius:"20px", width:"150px", height:"40px", fontWeight:"bold", fontSize:"25px"}}>
+    //     <Col style={{marginTop:"5px", }}>
+    //     {/* <Select value={selectedOption} options={scripts} onChange={()=>handleSelect()} style= {{backgroundColor:"transparent", color:"white", borderColor:"white",borderRadius:"20px", width:"10px", height:"40px", fontWeight:"bold", fontSize:"25px"}}> {getLanguageNameFromCode(props.script)} </Select> */}
+    //     {getLanguageNameFromCode(props.script)} 
+    //       </Col>
+    //     <Col style= {{marginTop:"-30px"}}>
+    //     <ExpandMoreIcon style={{color:"white", fontSize:"30px" ,marginLeft:"90px"}}/>
+    //     </Col>
+    //     </Button> */}
   const [selectedOption, setSelectedOption] = useState({Name:"N'ko", code:"Nko"});
+  const [isLanguagesOpen, setIsLanguagesOpen] = useState(false);
+  const dropdownMenu = {
+    menu:
+    <div
+								style={{
+                  backgroundColor:"white",
+									maxHeight: '300px',
+                  width:"150px",
+                  minHeight:"100px",
+
+									overflowY: 'scroll',
+									overflowX: 'hidden',
+									textOverflow: 'ellipsis',
+								}}
+							
+							>
+              <ul>
+								{ 
+                scripts.map(script => (
+                  <li>
+										<Link to={"/write" +`/${script.value}`+'/'}>
+											{script.label}
+										</Link>
+                    </li>
+									))}
+                  </ul>
+							</div>
+  }
     return (
-   
-     <Button style= {{backgroundColor:"transparent", color:"white", borderColor:"white",borderRadius:"20px", width:"150px", height:"40px", fontWeight:"bold", fontSize:"25px"}}>
-        <Col style={{marginTop:"5px", }}>
-        {/* <Select value={selectedOption} options={scripts} onChange={()=>handleSelect()} style= {{backgroundColor:"transparent", color:"white", borderColor:"white",borderRadius:"20px", width:"10px", height:"40px", fontWeight:"bold", fontSize:"25px"}}> {getLanguageNameFromCode(props.script)} </Select> */}
-        {getLanguageNameFromCode(props.script)} 
-          </Col>
-        <Col style= {{marginTop:"-30px"}}>
-        <ExpandMoreIcon style={{color:"white", fontSize:"30px" ,marginLeft:"90px"}}/>
-        </Col>
-        </Button>
+     <div>
+						<span
+							className='lgbo position-relative'
+							id='language-selector'
+							onClick={() =>
+								setIsLanguagesOpen(prevState => !prevState)
+							}
+						>
+							{getLanguageNameFromCode(props.script)}{' '}
+							{isLanguagesOpen ? (
+								<i><ExpandMoreIcon/></i>
+							) : (
+								<i><ExpandLessIcon/></i>
+							)}
+              {isLanguagesOpen? (<div>
+              {dropdownMenu.menu}
+
+              </div>):(<div>
+
+              </div>)}
+							
+						</span>{' '}
+</div>
   
-      
+  
     );
   }
   
---
+++ src/Scripts/scripts.js
@@ -0,0 +1,55 @@
+
+let scripts = [
+    {
+        label:"N'ko", 
+        value:"Nko"
+    }, 
+    {
+        label:"Adlam", 
+        value:"Adlam"
+    },
+    {
+        label:"Gʋlse", 
+        value:"Gʋlse"
+    },
+    {
+        label:"Nsibidi", 
+        value:"Nsibidi",
+    },
+    {
+        label:"Vai", 
+        value:"Vai",
+    },
+    {
+        label:"Bamum", 
+        value:"Bamum",
+    },
+    {
+        label:"Tifinagh", 
+        value:"Tifinagh",
+    },
+    {
+        label:"Bete", 
+        value:"Bete",
+    },
+    {
+        label:"isiBheqe", 
+        value:"isiBheqe",
+    },
+    {
+        label:"Osmanya", 
+        value:"Osmanya",
+    },
+    {
+        label:"Garay", 
+        value:"Garay",
+    },
+    {
+        label:"Taxi", 
+        value:"Taxi",
+    },
+
+
+]
+
+export default scripts;
